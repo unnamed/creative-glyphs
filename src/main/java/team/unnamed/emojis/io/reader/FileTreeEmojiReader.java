d86ce974f83191d55112a26e96257c13ccdd8651
yusshu <yushu@unnamed.team>
2021-09-18 15:50:23 -0500

    remove filename filter

--- src/main/java/team/unnamed/emojis/io/reader/FileTreeEmojiReader.java
+++ src/main/java/team/unnamed/emojis/io/reader/FileTreeEmojiReader.java
@@ -9,24 +9,8 @@ import java.io.InputStream;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class FileTreeEmojiReader {
-
-    /**
-     * Pattern for file names, also for extracting the
-     * emoji name (removing the file extension)
-     */
-    private static final Pattern NAME_PATTERN = Pattern.compile("^([A-Za-z_]{1,14})\\.mcemoji$");
-
-    /**
-     * Logger for this class, to log all important information.
-     * Exceptions won't be logged by this logger, only warnings
-     * and information.
-     */
-    private static final Logger LOGGER = Logger.getLogger(FileTreeEmojiReader.class.getSimpleName());
 
     /**
      * The delegated emoji reader, this is the actual responsible
@@ -55,14 +39,8 @@ public class FileTreeEmojiReader {
         Collection<Emoji> emojis = new HashSet<>();
 
         for (File file : files) {
-            Matcher matcher = NAME_PATTERN.matcher(file.getName());
-            if (matcher.matches()) {
-                try (InputStream input = new FileInputStream(file)) {
-                    emojis.addAll(reader.read(input));
-                }
-            } else {
-                LOGGER.warning("File '" + file.getName() + "' doesn't " +
-                        "match the filename pattern: " + NAME_PATTERN.pattern());
+            try (InputStream input = new FileInputStream(file)) {
+                emojis.addAll(reader.read(input));
             }
         }
 
