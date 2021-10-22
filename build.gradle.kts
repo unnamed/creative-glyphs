e46eadebaee9232eea2b8a3469d992e02b51788d
yusshu <yushu@unnamed.team>
2021-10-22 09:45:43 -0500

    fix: fix merge conflict with 004bd7

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 }
 
 dependencies {
-    val spigot = "io.papermc.paper:paper:1.17-R0.1-SNAPSHOT";
+    val spigot = "org.spigotmc:spigot:1.8.8-R0.1-SNAPSHOT";
     val annotations = "org.jetbrains:annotations:21.0.0";
 
     compileOnly(spigot)
