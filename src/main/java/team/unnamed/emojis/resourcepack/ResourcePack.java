5ce5383519b3e14fb20d0752accad30e097386e1
yusshu <yushu@unnamed.team>
2021-10-24 11:55:48 -0500

    chore: add Nullable annotation to ResourcePack#prompt

--- src/main/java/team/unnamed/emojis/resourcepack/ResourcePack.java
+++ src/main/java/team/unnamed/emojis/resourcepack/ResourcePack.java
@@ -1,15 +1,17 @@
 package team.unnamed.emojis.resourcepack;
 
+import org.jetbrains.annotations.Nullable;
+
 public class ResourcePack extends UrlAndHash {
 
     private final boolean required;
-    private final String prompt;
+    @Nullable private final String prompt;
 
     public ResourcePack(
             String url,
             String hash,
             boolean required,
-            String prompt
+            @Nullable String prompt
     ) {
         super(url, hash);
         this.required = required;
@@ -20,7 +22,7 @@ public class ResourcePack extends UrlAndHash {
         return required;
     }
 
-    public String getPrompt() {
+    public @Nullable String getPrompt() {
         return prompt;
     }
 
