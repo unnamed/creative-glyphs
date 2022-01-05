0aebc03f9b010438b458f6ba87dda9b6afda40f0
yusshu <yushu@unnamed.team>
2022-01-05 18:39:12 -0500

    feat: add a way to get emoji data by its charaacter

--- src/main/java/team/unnamed/emojis/EmojiRegistry.java
+++ src/main/java/team/unnamed/emojis/EmojiRegistry.java
@@ -9,14 +9,19 @@ import java.util.Map;
 public class EmojiRegistry {
 
     private Map<String, Emoji> registry = new HashMap<>();
+    private Map<Character, Emoji> characters = new HashMap<>();
 
-    @Nullable
-    public Emoji get(String name) {
+    public @Nullable Emoji get(String name) {
         return registry.get(name);
+    }
+
+    public @Nullable Emoji getByChar(char c) {
+        return characters.get(c);
     }
 
     public void add(Emoji emoji) {
         registry.put(emoji.getName(), emoji);
+        characters.put(emoji.getCharacter(), emoji);
     }
 
     public void update(Collection<Emoji> emojis) {
@@ -25,11 +30,15 @@ public class EmojiRegistry {
         // map with an inconsistent state for some nanoseconds
         // (I'm paranoid)
         Map<String, Emoji> newRegistry = new HashMap<>();
+        Map<Character, Emoji> newCharacters = new HashMap<>();
+
         for (Emoji emoji : emojis) {
             newRegistry.put(emoji.getName(), emoji);
+            newCharacters.put(emoji.getCharacter(), emoji);
         }
         // update the registry
         registry = newRegistry;
+        characters = newCharacters;
     }
 
     public Collection<Emoji> values() {
