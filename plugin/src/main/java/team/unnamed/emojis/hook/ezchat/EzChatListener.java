4dee24a0cd8f84b7432d907ef17935025aec38ee
yusshu <yushu@unnamed.team>
2023-01-16 14:33:10 -0500

    feat: update EzChat support to 3.0.3

--- plugin/src/main/java/team/unnamed/emojis/hook/ezchat/EzChatListener.java
+++ plugin/src/main/java/team/unnamed/emojis/hook/ezchat/EzChatListener.java
@@ -1,12 +1,7 @@
 package team.unnamed.emojis.hook.ezchat;
 
-import me.fixeddev.ezchat.EasyTextComponent;
 import me.fixeddev.ezchat.event.AsyncEzChatEvent;
-import me.fixeddev.ezchat.format.ChatFormat;
-import me.fixeddev.ezchat.format.ChatFormatSerializer;
-import net.md_5.bungee.api.chat.BaseComponent;
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
+import net.kyori.adventure.text.Component;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
@@ -16,51 +11,23 @@ import team.unnamed.emojis.format.processor.MessageProcessor;
 
 public class EzChatListener implements Listener {
 
-    private final ChatFormatSerializer formatSerializer
-            = new ChatFormatSerializer();
-
     private final EmojiStore registry;
-    private final MessageProcessor<String, BaseComponent[]> messageProcessor;
+    private final MessageProcessor<Component, Component> messageProcessor;
 
-    public EzChatListener(
-            Plugin plugin,
-            EmojiStore registry
-    ) {
+    public EzChatListener(Plugin plugin, EmojiStore registry) {
         this.registry = registry;
-        this.messageProcessor = MessageProcessor.stringToLegacyComponent(plugin);
+        this.messageProcessor = MessageProcessor.component(plugin);
     }
 
     @EventHandler
     public void onChat(AsyncEzChatEvent event) {
-        // cancel EzChat event
-        event.setCancelled(true);
-
-        Player sender = event.getPlayer();
-        ChatFormat format = event.getPlayerChatFormat();
-        String message = ChatFormatSerializer.color(format.getChatColor())
-                + event.getMessage();
-
-        if (sender.hasPermission("ezchat.color")) {
-            message = ChatColor.translateAlternateColorCodes('&', message);
-        }
-
-        BaseComponent[] messageComponent = messageProcessor.process(message, registry, Permissions.permissionTest(sender));
-
-        if (!format.isUsePlaceholderApi()) {
-            EasyTextComponent component = formatSerializer.constructJsonMessage(format, sender);
-            component.append(messageComponent);
-            BaseComponent[] builtComponent = component.build();
-
-            for (Player recipient : event.getRecipients()) {
-                recipient.spigot().sendMessage(builtComponent);
-            }
-        } else {
-            for (Player recipient : event.getRecipients()) {
-                EasyTextComponent component = formatSerializer.constructJsonMessage(format, sender, recipient);
-                component.append(messageComponent);
-                recipient.spigot().sendMessage(component.build());
-            }
-        }
+        event.setMessage(
+                messageProcessor.process(
+                        event.getMessage(),
+                        registry,
+                        Permissions.permissionTest(event.getPlayer())
+                )
+        );
     }
 
 }
