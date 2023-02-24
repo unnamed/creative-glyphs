a00736e8de4ebe40204450cf0375cd8d5e3e453d
yusshu <yushuverduras1309@gmail.com>
2023-02-24 00:30:19 -0500

    refactor: increase emoji name limit to 32
    Names were limited to 14 characters because we wanted to use player names to provide tab-completion for older versions. Minecraft 1.19.3 implemented a chat complentions feature and this is no longer required, so the emoji name limit can be increased (We will not provide tab-completion for versions minor than 1.19.3)

--- plugin/src/main/java/team/unnamed/emojis/format/EmojiFormat.java
+++ plugin/src/main/java/team/unnamed/emojis/format/EmojiFormat.java
@@ -14,11 +14,11 @@ public final class EmojiFormat {
     public static final char USAGE_START = ':';
     public static final char USAGE_END = ':';
 
-    public static final @RegExp String NAME_PATTERN_STR = "[a-z0-9_]{1,14}";
+    public static final @RegExp String NAME_PATTERN_STR = "[a-z0-9_]{1,32}";
     public static final @RegExp String PERMISSION_PATTERN_STR = "[A-Za-z0-9_.]*";
 
     // The exact same that EMOJI_NAME_PATTERN_STRING but accepting uppercase characters
-    public static final @RegExp String USAGE_PATTERN_STR = USAGE_START + "([A-Za-z0-9_]{1,14})" + USAGE_END;
+    public static final @RegExp String USAGE_PATTERN_STR = USAGE_START + "([A-Za-z0-9_]{1,32})" + USAGE_END;
 
     public static final Pattern PERMISSION_PATTERN = Pattern.compile(PERMISSION_PATTERN_STR);
     public static final Pattern NAME_PATTERN = Pattern.compile(NAME_PATTERN_STR);
