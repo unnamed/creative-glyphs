8f8574d691b6f4e13ec21ab9549ec951dcd2bdd5
yusshu <yushuverduras1309@gmail.com>
2023-05-06 15:57:44 -0500

    refactor: remove unused class LegacyEmojiRepresentationProvider

--- src/main/java/team/unnamed/emojis/format/representation/EmojiRepresentationProvider.java
+++ src/main/java/team/unnamed/emojis/format/representation/EmojiRepresentationProvider.java
@@ -1,7 +1,6 @@
 package team.unnamed.emojis.format.representation;
 
 import net.kyori.adventure.text.Component;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.plugin.Plugin;
 import team.unnamed.emojis.Emoji;
 
@@ -9,13 +8,8 @@ public interface EmojiRepresentationProvider<T> {
 
     T represent(Emoji emoji);
 
-
     static EmojiRepresentationProvider<Component> component(Plugin plugin) {
         return new ComponentEmojiRepresentationProvider(plugin);
-    }
-
-    static EmojiRepresentationProvider<TextComponent> legacyComponent(Plugin plugin) {
-        return new LegacyComponentEmojiRepresentationProvider(plugin);
     }
 
 }
