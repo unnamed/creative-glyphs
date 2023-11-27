2a7495db0735b2c8e5893480473c503d84ad7e05
yusshu <yushu@unnamed.team>
2023-11-26 21:28:07 -0500

    feat: new plugin GlyphRenderer

--- plugin/src/main/java/team/unnamed/creativeglyphs/plugin/ComponentGlyphRenderer.java
+++ plugin/src/main/java/team/unnamed/creativeglyphs/plugin/ComponentGlyphRenderer.java
@@ -1,102 +1,42 @@
 package team.unnamed.creativeglyphs.plugin;
 
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.TextComponent;
-import net.kyori.adventure.text.event.HoverEvent;
-import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.Tag;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
-import org.bukkit.configuration.ConfigurationSection;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
 import team.unnamed.creativeglyphs.Glyph;
 import team.unnamed.creativeglyphs.content.GlyphRenderer;
 
-import static net.kyori.adventure.text.minimessage.tag.resolver.TagResolver.resolver;
+import static java.util.Objects.requireNonNull;
 
-public final class ComponentGlyphRenderer
-        implements GlyphRenderer<Component> {
-
+/**
+ * A {@link GlyphRenderer} implementation that renders glyphs to {@link Component}s
+ * using the {@link MiniMessage} format and taking everything from the plugin's
+ * configuration.
+ */
+public final class ComponentGlyphRenderer implements GlyphRenderer<Component> {
     private final Plugin plugin;
-    private final boolean isMiniMessageAvailable;
-    private boolean infoSupportMiniMessage = true;
 
-    public ComponentGlyphRenderer(Plugin plugin) {
-        this.plugin = plugin;
-        this.isMiniMessageAvailable = isMiniMessageAvailable();
-
-        if (isMiniMessageAvailable) {
-            plugin.getLogger().info("MiniMessage format is supported by the server, " +
-                    "trying to use it for emoji replacement...");
-        }
+    public ComponentGlyphRenderer(final @NotNull Plugin plugin) {
+        this.plugin = requireNonNull(plugin, "plugin");
     }
 
     @Override
     public Component render(Glyph glyph) {
+        final String format = plugin.getConfig().getString("emoji-format", null);
 
-        ConfigurationSection config = plugin.getConfig();
-
-        Component hoverComponent = null;
-        if (isMiniMessageAvailable) {
-            String source = config.getString("format.hover.mini-message", null);
-            if (source != null) {
-                hoverComponent = MiniMessage.miniMessage().deserialize(
-                        source,
-                        resolver("emoji", Tag.selfClosingInserting(emojiComponent(glyph))),
-                        resolver("emojiname", Tag.inserting(Component.text(glyph.name())))
-                );
-            } else if (infoSupportMiniMessage
-                    && (config.getString("format.hover.legacy", null) != null
-                    || config.getString("format.paper.emoji", null) != null)) {
-                // MiniMessage is supported but mini-message format is not set, warn
-                plugin.getLogger().warning("MiniMessage format is supported by the server, " +
-                        "but you are not using it, we recommend using it " +
-                        "(De-comment 'format.hover.mini-message' in config.yml)");
-                infoSupportMiniMessage = false;
-            }
+        if (format == null) {
+            plugin.getLogger().warning("'emoji-format' is not set in the config.yml," +
+                    " emoji rendering may not work as expected. This may be caused by an " +
+                    " update that changed the configuration structure, please check the plugin's" +
+                    " documentation and change-log for more information.");
+            return Component.text(glyph.replacement());
         }
 
-        if (hoverComponent == null) {
-            String legacyFormat = config.getString("format.hover.legacy", null);
-
-            if (legacyFormat == null) {
-                // Try with the old path (format.paper.emoji)
-                // for backwards compatibility
-                legacyFormat = config.getString("format.paper.emoji", null);
-            }
-
-            if (legacyFormat != null) {
-                hoverComponent = LegacyComponentSerializer.legacyAmpersand().deserialize(
-                        legacyFormat
-                                .replace("<emoji>", glyph.replacement())
-                                .replace("<emojiname>", glyph.name())
-                );
-            }
-        }
-
-        TextComponent.Builder builder = emojiComponent(glyph);
-
-        // Using a hover text for emojis is optional
-        if (hoverComponent != null) {
-            builder.hoverEvent(HoverEvent.showText(hoverComponent));
-        }
-
-        return builder.build();
+        return MiniMessage.miniMessage().deserialize(format,
+                TagResolver.resolver("emoji", Tag.preProcessParsed(glyph.replacement())),
+                TagResolver.resolver("emojiname", Tag.preProcessParsed(glyph.name())));
     }
-
-    private TextComponent.Builder emojiComponent(Glyph glyph) {
-        return Component.text()
-                .color(NamedTextColor.WHITE) // text color affects character texture
-                .content(glyph.replacement()); // use the emoji character
-    }
-
-    private static boolean isMiniMessageAvailable() {
-        try {
-            Class.forName("net.kyori.adventure.text.minimessage.MiniMessage");
-            return true;
-        } catch (ClassNotFoundException e) {
-            return false;
-        }
-    }
-
 }
