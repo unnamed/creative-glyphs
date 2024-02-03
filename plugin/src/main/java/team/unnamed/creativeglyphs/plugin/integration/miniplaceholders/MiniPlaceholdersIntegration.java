4a76b47fb16ce6dd837d4a6f6aac3e084feaa0ff
yusshu <yushu@unnamed.team>
2024-02-02 21:04:04 -0500

    *: (MiniPlaceholders Integration) deprecate 'unemoji', add 'creativelglyphs'

--- plugin/src/main/java/team/unnamed/creativeglyphs/plugin/integration/miniplaceholders/MiniPlaceholdersIntegration.java
+++ plugin/src/main/java/team/unnamed/creativeglyphs/plugin/integration/miniplaceholders/MiniPlaceholdersIntegration.java
@@ -2,18 +2,26 @@ package team.unnamed.creativeglyphs.plugin.integration.miniplaceholders;
 
 import io.github.miniplaceholders.api.Expansion;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.Context;
 import net.kyori.adventure.text.minimessage.tag.Tag;
+import net.kyori.adventure.text.minimessage.tag.resolver.ArgumentQueue;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 import team.unnamed.creativeglyphs.Glyph;
+import team.unnamed.creativeglyphs.plugin.CreativeGlyphsPlugin;
 import team.unnamed.creativeglyphs.plugin.integration.PluginIntegration;
 import team.unnamed.creativeglyphs.plugin.PluginGlyphMap;
 
-public final class MiniPlaceholdersIntegration implements PluginIntegration {
-    private final PluginGlyphMap registry;
+import java.util.function.BiFunction;
 
-    public MiniPlaceholdersIntegration(final PluginGlyphMap registry) {
-        this.registry = registry;
+import static java.util.Objects.requireNonNull;
+
+public final class MiniPlaceholdersIntegration implements PluginIntegration {
+    private final CreativeGlyphsPlugin plugin;
+    private boolean warnAboutDeprecation = true;
+
+    public MiniPlaceholdersIntegration(final @NotNull CreativeGlyphsPlugin plugin) {
+        this.plugin = requireNonNull(plugin, "plugin");
     }
 
     @Override
@@ -22,16 +30,30 @@ public final class MiniPlaceholdersIntegration implements PluginIntegration {
     }
 
     @Override
-    public void enable(final @NotNull Plugin hook) {
+    public void enable(final @NotNull Plugin miniPlaceholders) {
+        final var placeholder = (BiFunction<ArgumentQueue, Context, Tag>) (queue, ctx) -> {
+            final var argument = queue.popOr("You need to provide an argument");
+            final var glyph = plugin.registry().getByName(argument.value());
+            if (glyph == null) {
+                return null;
+            } else {
+                return Tag.selfClosingInserting(Component.text(glyph.replacement()));
+            }
+        };
+
+        Expansion.builder("creativeglyphs")
+                .globalPlaceholder("glyph", placeholder)
+                .build()
+                .register();
+
         Expansion.builder("unemoji")
                 .globalPlaceholder("emoji", (queue, ctx) -> {
-                    final Tag.Argument argument = queue.popOr("You need to provide an argument");
-                    Glyph glyph = registry.getByName(argument.value());
-                    if (glyph == null) {
-                        return null;
-                    } else {
-                        return Tag.selfClosingInserting(Component.text(glyph.replacement()));
+                    if (warnAboutDeprecation) {
+                        plugin.getLogger().warning("(MiniPlaceholders Integration) Detected usage of deprecated" +
+                                " placeholder format: '<unemoji_emoji:NAME>', please use '<creativeglyphs_glyph:NAME>' instead.");
+                        warnAboutDeprecation = false;
                     }
+                    return placeholder.apply(queue, ctx);
                 })
                 .build()
                 .register();
